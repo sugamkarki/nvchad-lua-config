752f6120eeebdd3f328ecc9dbc5b6edfe7ca0d08
Sugam Karki <sugamkarki7058@gmail.com>
2024-12-03 10:09:40 -0700

    added tsserver

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -12,6 +12,16 @@ return {
       require "configs.lspconfig"
     end,
   },
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end
+}
 
   -- {
   -- 	"nvim-treesitter/nvim-treesitter",
