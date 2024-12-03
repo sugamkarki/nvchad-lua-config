3b90fb0597607a7d00da408c066321e2ba01b9a3
Sugam Karki <sugamkarki7058@gmail.com>
2024-12-02 23:38:08 -0700

    INIT COMMIT

---
+++ lua/chadrc.lua
@@ -0,0 +1,19 @@
+-- This file needs to have same structure as nvconfig.lua 
+-- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
+-- Please read that file to know all available options :( 
+
+---@type ChadrcConfig
+local M = {}
+
+M.base46 = {
+	theme = "onedark",
+
+	-- hl_override = {
+	-- 	Comment = { italic = true },
+	-- 	["@comment"] = { italic = true },
+	-- },
+}
+
+-- Map 'jk' to escape in normal mode
+vim.api.nvim_set_keymap('i', 'jk', '<Esc>', { noremap = true, silent = true })
+return M
